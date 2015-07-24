1cf9fe844d6665a5902159d3382e9e9e286c6c2d
Leonard Ng'eno <dranoel.bravo@gmail.com>
2015-07-24 12:41:02 -0400

    Import project from Eclipse to Android Studio

--- build/source/buildConfig/debug/org/adaptlab/chpir/android/survey/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.adaptlab.chpir.android.survey;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
