102ecce22f97de22ce37755b881800024394feab
Leonard Ng'eno <dranoel.bravo@gmail.com>
2015-08-26 16:35:07 -0400

    Add OS build number to device sync entry

--- app/src/main/java/org/adaptlab/chpir/android/survey/Models/DeviceSyncEntry.java
+++ app/src/main/java/org/adaptlab/chpir/android/survey/Models/DeviceSyncEntry.java
@@ -1,7 +1,7 @@
 package org.adaptlab.chpir.android.survey.Models;
 
-import java.util.Locale;
-import java.util.TimeZone;
+import android.content.Context;
+import android.util.Log;
 
 import org.adaptlab.chpir.android.activerecordcloudsync.SendModel;
 import org.adaptlab.chpir.android.survey.AppUtil;
@@ -9,13 +9,13 @@ import org.adaptlab.chpir.android.survey.Location.LocationServiceManager;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.content.Context;
-import android.util.Log;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class DeviceSyncEntry extends SendModel {
     private static final String TAG = "DeviceSyncEntry";
     private LocationServiceManager mLocationServiceManager;
-    
+
     public DeviceSyncEntry() {
         mLocationServiceManager = LocationServiceManager.get(AppUtil.getContext());
         mLocationServiceManager.startLocationUpdates();
@@ -25,15 +25,16 @@ public class DeviceSyncEntry extends SendModel {
     public JSONObject toJSON() {
         Log.i(TAG, "Creating JSON for " + TAG);
         JSONObject json = new JSONObject();
-        
+
         try {
             JSONObject jsonObject = new JSONObject();
-            
+
             jsonObject.put("latitude", mLocationServiceManager.getLatitude());
             jsonObject.put("longitude", mLocationServiceManager.getLongitude());
             jsonObject.put("current_version_code", AppUtil.getVersionCode(AppUtil.getContext()));
             jsonObject.put("current_version_name", AppUtil.getVersionName(AppUtil.getContext()));
-            jsonObject.put("num_surveys", Survey.getAll().size());
+            jsonObject.put("num_complete_surveys", Survey.getCompleted().size());
+            jsonObject.put("num_incomplete_surveys", Survey.getIncomplete().size());
             jsonObject.put("current_language", Locale.getDefault().getDisplayLanguage());
             jsonObject.put("instrument_versions", instrumentVersions().toString());
             jsonObject.put("device_uuid", AdminSettings.getInstance().getDeviceIdentifier());
@@ -41,42 +42,50 @@ public class DeviceSyncEntry extends SendModel {
             jsonObject.put("timezone", TimeZone.getDefault().getDisplayName() + " " + TimeZone.getDefault().getID());
             jsonObject.put("project_id", AdminSettings.getInstance().getProjectId());
             jsonObject.put("device_label", AdminSettings.getInstance().getDeviceLabel());
-            
+            jsonObject.put("os_build_number", AppUtil.getOsBuildNumber());
+
             json.put("device_sync_entry", jsonObject);
         } catch (JSONException je) {
             Log.e(TAG, "JSON exception", je);
         }
-        
+
         return json;
     }
 
     @Override
-    public boolean isSent() { return false; }
+    public boolean isSent() {
+        return false;
+    }
 
     @Override
-    public boolean readyToSend() { return true; }
+    public boolean readyToSend() {
+        return true;
+    }
 
     @Override
-    public void setAsSent(Context context) { }
-    
+    public void setAsSent(Context context) {
+    }
+
     @Override
-    public boolean isPersistent() { return false; }
-    
+    public boolean isPersistent() {
+        return false;
+    }
+
     private JSONObject instrumentVersions() {
         JSONObject json = new JSONObject();
-        
+
         try {
             JSONObject jsonObject = new JSONObject();
-            
+
             for (Instrument instrument : Instrument.getAll()) {
                 jsonObject.put(instrument.getTitle(), instrument.getVersionNumber());
             }
-            
+
             json.put("instrument_versions", jsonObject);
         } catch (JSONException je) {
             Log.e(TAG, "JSON exception", je);
         }
-        
+
         return json;
     }
 }
--- app/src/main/java/org/adaptlab/chpir/android/survey/Models/Survey.java
+++ app/src/main/java/org/adaptlab/chpir/android/survey/Models/Survey.java
@@ -1,15 +1,5 @@
 package org.adaptlab.chpir.android.survey.Models;
 
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
-
-import org.adaptlab.chpir.android.activerecordcloudsync.SendModel;
-import org.adaptlab.chpir.android.survey.AppUtil;
-import org.adaptlab.chpir.android.survey.R;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
@@ -21,6 +11,16 @@ import android.util.Log;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
 import com.activeandroid.query.Select;
+
+import org.adaptlab.chpir.android.activerecordcloudsync.SendModel;
+import org.adaptlab.chpir.android.survey.AppUtil;
+import org.adaptlab.chpir.android.survey.R;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 @Table(name = "Surveys")
 public class Survey extends SendModel {
@@ -36,7 +36,7 @@ public class Survey extends SendModel {
     private boolean mComplete;
     @Column(name = "Latitude")
     private String mLatitude;
-    @Column(name= "Longitude")
+    @Column(name = "Longitude")
     private String mLongitude;
     @Column(name = "LastUpdated")
     private Date mLastUpdated;
@@ -54,11 +54,11 @@ public class Survey extends SendModel {
         mUUID = UUID.randomUUID().toString();
         mLastUpdated = new Date();
     }
-    
+
     @Override
     public JSONObject toJSON() {
         JSONObject json = new JSONObject();
-        
+
         try {
             JSONObject jsonObject = new JSONObject();
             jsonObject.put("instrument_id", getInstrument().getRemoteId());
@@ -70,68 +70,84 @@ public class Survey extends SendModel {
             jsonObject.put("latitude", mLatitude);
             jsonObject.put("longitude", mLongitude);
             jsonObject.put("metadata", mMetadata);
-            
+
             json.put("survey", jsonObject);
         } catch (JSONException je) {
             Log.e(TAG, "JSON exception", je);
         }
         return json;
     }
-    
+
     public String getAdminInstanceDeviceIdentifier() {
-    	return AppUtil.getAdminSettingsInstance().getDeviceIdentifier();
+        return AppUtil.getAdminSettingsInstance().getDeviceIdentifier();
     }
-    
+
     @Override
-    public boolean isPersistent() { return true; }
-    
+    public boolean isPersistent() {
+        return true;
+    }
+
     /*
      * The identifier to display to the user to identify a Survey.
      * Return Unidentified Survey string if no response for identifier questions.
      */
     public String identifier(Context context) {
-    	String surveyLabel = null;
-    	String identifier = "";
-    	
-    	if (!TextUtils.isEmpty(getMetadata())) { surveyLabel = getMetadataLabel(); }    	
-    	if (!TextUtils.isEmpty(surveyLabel))  { return surveyLabel; }
-    	
-    	for (Response response : responses()) {
-    		if (response.getQuestion().identifiesSurvey()) {
-    			identifier += response.getText() + " ";
-    		}
-    	}
-    	
-    	if (identifier.trim().isEmpty())
-    		return context.getString(R.string.unidentified_survey) + " " + getId();
-    	else
-    		return identifier;
+        String surveyLabel = null;
+        String identifier = "";
+
+        if (!TextUtils.isEmpty(getMetadata())) {
+            surveyLabel = getMetadataLabel();
+        }
+        if (!TextUtils.isEmpty(surveyLabel)) {
+            return surveyLabel;
+        }
+
+        for (Response response : responses()) {
+            if (response.getQuestion().identifiesSurvey()) {
+                identifier += response.getText() + " ";
+            }
+        }
+
+        if (identifier.trim().isEmpty())
+            return context.getString(R.string.unidentified_survey) + " " + getId();
+        else
+            return identifier;
     }
-    
+
     /*
      * Finders
-     */   
+     */
     public Response getResponseByQuestion(Question question) {
         return new Select().from(Response.class).where(
                 "Question = ? AND Survey = ?",
                 question.getId(),
                 getId()).executeSingle();
     }
-    
+
     public static List<Survey> getAll() {
         return new Select().from(Survey.class).orderBy("LastUpdated DESC").execute();
     }
 
     public static List<Survey> getAllProjectSurveys(Long projectId) {
-    	return new Select("Surveys.*")
-    			.from(Survey.class)
-    			.innerJoin(Instrument.class)
-    			.on("Surveys.Instrument=Instruments.Id AND Instruments.Published=" + 1)
-    			.where("Surveys.ProjectId = ?", projectId)
-    			.orderBy("Surveys.LastUpdated DESC")
-    			.execute();
+        return new Select("Surveys.*")
+                .from(Survey.class)
+                .innerJoin(Instrument.class)
+                .on("Surveys.Instrument=Instruments.Id AND Instruments.Published=" + 1)
+                .where("Surveys.ProjectId = ?", projectId)
+                .orderBy("Surveys.LastUpdated DESC")
+                .execute();
     }
-    
+
+    public static List<Survey> getCompleted() {
+        return new Select().from(Survey.class).where("Complete = ? AND ProjectID = ?", 1,
+                Long.valueOf(AppUtil.getAdminSettingsInstance().getProjectId())).execute();
+    }
+
+    public static List<Survey> getIncomplete() {
+        return new Select().from(Survey.class).where("Complete = ? AND ProjectID = ?", 0,
+                Long.valueOf(AppUtil.getAdminSettingsInstance().getProjectId())).execute();
+    }
+
     /*
      * Relationships
      */
@@ -150,105 +166,105 @@ public class Survey extends SendModel {
     public void setInstrument(Instrument instrument) {
         mInstrument = instrument;
     }
-    
+
     public String getUUID() {
         return mUUID;
     }
-    
+
     public void setAsComplete() {
         mComplete = true;
     }
-    
+
     @Override
     public boolean isSent() {
         return mSent;
     }
-    
+
     @Override
     public void setAsSent(Context context) {
         mSent = true;
         this.save();
-        
+
         EventLog eventLog = new EventLog(EventLog.EventType.SENT_SURVEY, context);
         eventLog.setInstrument(mInstrument);
         eventLog.setSurveyIdentifier(identifier(context));
         eventLog.save();
-        
+
         Resources r = context.getResources();
 
         Notification notification = new NotificationCompat.Builder(context)
-            .setTicker(r.getString(R.string.app_name))
-            .setSmallIcon(android.R.drawable.ic_dialog_info)
-            .setContentTitle(r.getString(R.string.app_name))
-            .setContentText(eventLog.getLogMessage(context))
-            .setAutoCancel(true)
-            .build();
-        
+                .setTicker(r.getString(R.string.app_name))
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(r.getString(R.string.app_name))
+                .setContentText(eventLog.getLogMessage(context))
+                .setAutoCancel(true)
+                .build();
+
         NotificationManager notificationManager = (NotificationManager)
                 context.getSystemService(Context.NOTIFICATION_SERVICE);
-        
+
         notificationManager.notify(eventLog.getLogMessage(context), 1, notification);
     }
-    
+
     @Override
     public boolean readyToSend() {
         return mComplete;
     }
-    
+
     public void setLatitude(String latitude) {
-    	mLatitude = latitude;
+        mLatitude = latitude;
     }
-    
+
     public String getLatitude() {
-    	return mLatitude;
+        return mLatitude;
     }
-    
+
     public void setLongitude(String longitude) {
-    	mLongitude = longitude;
+        mLongitude = longitude;
     }
-    
+
     public String getLongitude() {
-    	return mLongitude;
+        return mLongitude;
     }
-    
+
     public Date getLastUpdated() {
         return mLastUpdated;
     }
-    
+
     public void setLastUpdated(Date lastUpdate) {
         mLastUpdated = lastUpdate;
     }
-    
+
     public Question getLastQuestion() {
         return mLastQuestion;
     }
-    
+
     public void setLastQuestion(Question question) {
         mLastQuestion = question;
     }
-    
+
     public void deleteIfComplete() {
-		if (this.responses().size() == 0) {
-			this.delete();
-		}
+        if (this.responses().size() == 0) {
+            this.delete();
+        }
     }
-    
+
     public void setMetadata(String metadata) {
         mMetadata = metadata;
     }
-    
+
     public String getMetadata() {
         return mMetadata;
     }
-    
+
     public void setProjectId(Long id) {
-    	mProjectId = id;
+        mProjectId = id;
     }
-    
+
     public Long getProjectId() {
-    	return mProjectId;
+        return mProjectId;
     }
-    
+
     private String getMetadataLabel() {
         try {
             JSONObject metadata = new JSONObject(getMetadata());
@@ -258,7 +274,7 @@ public class Survey extends SendModel {
         } catch (JSONException er) {
             Log.e(TAG, er.getMessage());
         }
-        
+
         return "";
     }
 }
