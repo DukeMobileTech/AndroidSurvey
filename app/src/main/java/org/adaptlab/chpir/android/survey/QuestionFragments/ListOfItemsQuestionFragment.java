5f7e097188b1e582befca3129d27565c002afe3e
Leonard Ng'eno <dranoel.bravo@gmail.com>
2015-09-28 11:22:17 -0400

    escape strings when generating csv strings

--- app/src/main/java/org/adaptlab/chpir/android/survey/QuestionFragments/ListOfItemsQuestionFragment.java
+++ app/src/main/java/org/adaptlab/chpir/android/survey/QuestionFragments/ListOfItemsQuestionFragment.java
@@ -1,18 +1,29 @@
 package org.adaptlab.chpir.android.survey.QuestionFragments;
 
-import java.util.ArrayList;
-
-import org.adaptlab.chpir.android.survey.QuestionFragment;
-import org.adaptlab.chpir.android.survey.R;
-import org.adaptlab.chpir.android.survey.Models.Option;
-
+import android.annotation.SuppressLint;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.opencsv.CSVReader;
+
+import org.adaptlab.chpir.android.survey.Models.Option;
+import org.adaptlab.chpir.android.survey.QuestionFragment;
+import org.adaptlab.chpir.android.survey.R;
+import org.apache.commons.codec.Charsets;
+import org.apache.commons.lang3.StringEscapeUtils;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public abstract class ListOfItemsQuestionFragment extends QuestionFragment {
+    private static final String TAG = "ListOfItemsQuestionFragment";
     private ArrayList<EditText> mResponses;
     protected abstract EditText createEditText();
     
@@ -45,19 +56,28 @@ public abstract class ListOfItemsQuestionFragment extends QuestionFragment {
     protected String serialize() {
         String serialized = "";
         for (int i = 0; i < mResponses.size(); i++) {
-            serialized += mResponses.get(i).getText().toString();
+            serialized += StringEscapeUtils.escapeCsv(mResponses.get(i).getText().toString());
             if (i <  mResponses.size() - 1) serialized += LIST_DELIMITER;
         }
         return serialized;
     }
 
+    @SuppressLint("LongLogTag")
     @Override
     protected void deserialize(String responseText) {
         if (responseText.equals("")) return;    
-        String[] listOfResponses = responseText.split(LIST_DELIMITER);
-        for (int i = 0; i < listOfResponses.length; i++) {
-            if (mResponses.size() > i)
-                mResponses.get(i).setText(listOfResponses[i]);
+        InputStream input = new ByteArrayInputStream(responseText.getBytes(Charsets.UTF_8));
+        InputStreamReader inputReader = new InputStreamReader(input);
+        CSVReader reader = new CSVReader(inputReader);
+        String[] listOfResponses;
+        try {
+            listOfResponses = reader.readNext();
+            for (int i = 0; i < listOfResponses.length; i++) {
+                if (mResponses.size() > i)
+                    mResponses.get(i).setText(listOfResponses[i]);
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "IOException " + e.getMessage());
         }
     }
 }
