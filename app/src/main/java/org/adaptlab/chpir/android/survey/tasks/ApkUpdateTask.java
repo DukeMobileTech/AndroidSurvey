09f9210054000efe26d453f11843823e5e036a08
Leonard Ng'eno <dranoel.bravo@gmail.com>
2017-11-16 10:23:02 -0500

    show progress dialog when downloading file

--- app/src/main/java/org/adaptlab/chpir/android/survey/tasks/ApkUpdateTask.java
+++ app/src/main/java/org/adaptlab/chpir/android/survey/tasks/ApkUpdateTask.java
@@ -3,6 +3,7 @@ package org.adaptlab.chpir.android.survey.tasks;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -47,8 +48,7 @@ public class ApkUpdateTask extends AsyncTask<Void, Void, Void> {
 
     @Override
     protected void onPreExecute() {
-        Toast.makeText(mContext, R.string.background_process_progress_message,
-                Toast.LENGTH_SHORT).show();
+        Toast.makeText(mContext, R.string.background_process_progress_message, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -62,8 +62,7 @@ public class ApkUpdateTask extends AsyncTask<Void, Void, Void> {
     private void checkLatestApk() {
         ActiveRecordCloudSync.setAccessToken(AppUtil.getAdminSettingsInstance().getApiKey());
         ActiveRecordCloudSync.setVersionCode(AppUtil.getVersionCode(mContext));
-        String url = AppUtil.getAdminSettingsInstance().getApiUrl() + "android_updates" +
-                ActiveRecordCloudSync.getParams();
+        String url = AppUtil.getAdminSettingsInstance().getApiUrl() + "android_updates" + ActiveRecordCloudSync.getParams();
         try {
             String jsonString = getUrl(url);
             if (AppUtil.DEBUG) Log.i(TAG, "Got JSON String: " + jsonString);
@@ -72,9 +71,7 @@ public class ApkUpdateTask extends AsyncTask<Void, Void, Void> {
                 mLatestVersion = obj.getInt("version");
                 mApkId = obj.getInt("id");
                 mFileName = UUID.randomUUID().toString() + ".apk";
-                if (AppUtil.DEBUG)
-                    Log.i(TAG, "Latest version is: " + mLatestVersion + ". Old version is: " +
-                            AppUtil.getVersionCode(mContext));
+                if (AppUtil.DEBUG) Log.i(TAG, "Latest version is: " + mLatestVersion + ". Old version is: " + AppUtil.getVersionCode(mContext));
             }
         } catch (ConnectException cre) {
             if (AppUtil.DEBUG) Log.e(TAG, "Connection was refused", cre);
@@ -96,16 +93,11 @@ public class ApkUpdateTask extends AsyncTask<Void, Void, Void> {
                         .setMessage(R.string.new_apk_message)
                         .setPositiveButton(R.string.okay, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int button) {
-                                if (ContextCompat.checkSelfPermission(mContext,
-                                        Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
-                                        PackageManager.PERMISSION_DENIED) {
-                                    ActivityCompat.requestPermissions((Activity) mContext,
-                                            new String[]{Manifest.permission.
-                                                    WRITE_EXTERNAL_STORAGE}, 1);
-                                    Toast.makeText(mContext, R.string.re_check_for_updates,
-                                            Toast.LENGTH_LONG).show();
+                                if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                                    ActivityCompat.requestPermissions((Activity) mContext, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                                    Toast.makeText(mContext, R.string.re_check_for_updates, Toast.LENGTH_LONG).show();
                                 } else {
-                                    new DownloadApkTask().execute();
+                                    new DownloadApkTask(mContext).execute();
                                 }
                             }
                         })
@@ -146,7 +138,35 @@ public class ApkUpdateTask extends AsyncTask<Void, Void, Void> {
         }
     }
 
-    private class DownloadApkTask extends AsyncTask<Void, Void, Void> {
+
+    private class DownloadApkTask extends AsyncTask<Void, Integer, Void> {
+        private ProgressDialog progressDialog;
+        private Context mContext;
+        private double contentLength = 0.0;
+        private int bytesRead = 0;
+        private double total = 0.0;
+
+        private DownloadApkTask(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = new ProgressDialog(mContext);
+            progressDialog.setTitle(mContext.getString(R.string.instrument_loading_progress_header));
+            progressDialog.setMessage(mContext.getString(R.string.background_process_progress_message));
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            progressDialog.setProgress(0);
+            progressDialog.setCancelable(false);
+            progressDialog.setMax(100);
+            progressDialog.show();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            progressDialog.setProgress(values[0]);
+        }
 
         @Override
         protected Void doInBackground(Void... params) {
@@ -158,33 +178,62 @@ public class ApkUpdateTask extends AsyncTask<Void, Void, Void> {
 
         @Override
         protected void onPostExecute(Void param) {
-            Intent intent = new Intent();
-            intent.setAction(Intent.ACTION_VIEW);
-            intent.setDataAndType(Uri.fromFile(mFile), "application/vnd.android.package-archive");
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            mContext.startActivity(intent);
+            if (progressDialog != null && progressDialog.isShowing()) {
+                progressDialog.dismiss();
+                progressDialog = null;
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setDataAndType(Uri.fromFile(mFile), "application/vnd.android.package-archive");
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                mContext.startActivity(intent);
+            }
         }
 
         private void downloadLatestApk() {
-            String url = AppUtil.getAdminSettingsInstance().getApiUrl() + "android_updates/" +
-                    mApkId + "/" + ActiveRecordCloudSync.getParams();
-            File path = Environment.getExternalStoragePublicDirectory(Environment
-                    .DIRECTORY_DOWNLOADS);
+            String url = AppUtil.getAdminSettingsInstance().getApiUrl() + "android_updates/" + mApkId + "/" + ActiveRecordCloudSync.getParams();
+            File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
             mFile = new File(path, mFileName);
-            FileOutputStream filewriter = null;
+            FileOutputStream fileWriter = null;
             try {
-                byte[] imageBytes = getUrlBytes(url);
-                filewriter = new FileOutputStream(mFile);
-                filewriter.write(imageBytes);
+                byte[] imageBytes = getBytes(url);
+                fileWriter = new FileOutputStream(mFile);
+                fileWriter.write(imageBytes);
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
                 try {
-                    if (filewriter != null)
-                        filewriter.close();
+                    if (fileWriter != null)
+                        fileWriter.close();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+            }
+        }
+
+        private byte[] getBytes(String urlSpec) throws IOException {
+            URL url = new URL(urlSpec);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            try {
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                InputStream in = connection.getInputStream();
+                contentLength = connection.getContentLength();
+                if (contentLength == -1) contentLength = 5000000.0; // File size unknown
+
+                if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    return null;
+                }
+
+                byte[] buffer = new byte[1024];
+                while ((bytesRead = in.read(buffer)) > 0) {
+                    total += bytesRead;
+                    int progress = ((int) ((total / contentLength) * 100));
+                    publishProgress(progress);
+                    out.write(buffer, 0, bytesRead);
+                }
+                out.close();
+                return out.toByteArray();
+            } finally {
+                connection.disconnect();
             }
         }
 
