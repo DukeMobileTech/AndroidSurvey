d67672b349aa3119a214deef75eebe857e9578c8
Leonard Ng'eno <dranoel.bravo@gmail.com>
2018-08-28 13:53:06 -0400

    show selected date for calendar view

--- app/src/main/java/org/adaptlab/chpir/android/survey/questionfragments/DateQuestionFragment.java
+++ app/src/main/java/org/adaptlab/chpir/android/survey/questionfragments/DateQuestionFragment.java
@@ -1,16 +1,26 @@
 package org.adaptlab.chpir.android.survey.questionfragments;
 
+import android.graphics.Typeface;
+import android.os.Build;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.DatePicker.OnDateChangedListener;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import org.adaptlab.chpir.android.survey.R;
 import org.adaptlab.chpir.android.survey.utils.FormatUtils;
 import org.adaptlab.chpir.android.survey.SingleQuestionFragment;
 import org.adaptlab.chpir.android.survey.views.ScrollableDatePicker;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class DateQuestionFragment extends SingleQuestionFragment {
@@ -18,31 +28,53 @@ public class DateQuestionFragment extends SingleQuestionFragment {
     protected int mMonth;
     protected int mYear;
     private DatePicker mDatePicker;
+    private TextView mSelectedDate;
+    private OnDateChangedListener mListener = new OnDateChangedListener() {
+        @Override
+        public void onDateChanged(DatePicker view, int newYear, int newMonth, int newDay) {
+            mDay = newDay;
+            mMonth = newMonth;
+            mYear = newYear;
+            setResponse(null);
+            setSelectedDate();
+            if (mSpecialResponses != null) {
+                mSpecialResponses.clearCheck();
+            }
+        }
+    };
 
     protected DatePicker beforeAddViewHook(ViewGroup component) {
         DatePicker datePicker = new ScrollableDatePicker(getActivity());
+        LinearLayout linearLayout = new LinearLayout(getActivity());
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT);
-        params.gravity = Gravity.CENTER;
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.weight = 3;
         datePicker.setLayoutParams(params);
         datePicker.setCalendarViewShown(false);
         Calendar c = Calendar.getInstance();
         datePicker.init(c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH),
-                new OnDateChangedListener() {
-                    @Override
-                    public void onDateChanged(DatePicker view, int newYear, int newMonth,
-                                              int newDay) {
-                        mDay = newDay;
-                        mMonth = newMonth;
-                        mYear = newYear;
-                        setResponse(null);
-                        if (mSpecialResponses != null) {
-                            mSpecialResponses.clearCheck();
-                        }
-                    }
-                });
-        component.addView(datePicker);
+                mListener);
+        linearLayout.addView(datePicker);
+        mSelectedDate = new TextView(getActivity());
+        setSelectedDate();
+        linearLayout.addView(mSelectedDate);
+        component.addView(linearLayout);
         return datePicker;
+    }
+
+    private void setSelectedDate() {
+        String dateTitle = getString(R.string.selected_date);
+        String string = dateTitle + "\n" + mResponse.getText();
+        SpannableString spannableString = new SpannableString(string);
+        spannableString.setSpan(new StyleSpan(Typeface.BOLD), dateTitle.length(), string.length(),
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color
+                .primary_text)), 0, dateTitle.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        mSelectedDate.setText(spannableString);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.weight = 1;
+        mSelectedDate.setLayoutParams(params);
     }
 
     @Override
@@ -70,7 +102,11 @@ public class DateQuestionFragment extends SingleQuestionFragment {
     protected void unSetResponse() {
         if (mResponse != null) {
             mResponse.setResponse("");
+            setSelectedDate();
         }
+        Calendar calendar = Calendar.getInstance();
+        mDatePicker.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH), mListener);
     }
 
 }
